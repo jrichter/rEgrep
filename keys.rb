ac0e7d111cedd6177da56617a6892d8846f5a23c
Justin Richter <jrichter@jetfive.com>
2011-11-25 12:18:05 -0600

    pass files through ARGV and colorize output

--- keys.rb
+++ keys.rb
@@ -1,14 +1,28 @@
 #! /home/justin/.rvm/rubies/ruby-1.9.3-preview1/bin/ruby -w
+require 'colorize'
 
-keybinding_file = "~/.emacs.d/modules/prelude-global-keybindings.el"
+keybinding_files = ARGV
+files = ""
+keybinding_files.each {|file| files += File.basename(file) + " "}
 
-while true
+puts %$Searching "#{files.strip}"$.blue
+puts "quit! to exit \n \n"
+puts "Enter Search Term".green
 
-  puts %$ Searching "#{File.basename(keybinding_file)}" - quit! to exit $
-  term = gets
+while term = STDIN.gets
   term = term.chomp
   break if term == "quit!"
 
-  puts `egrep -B 2 -A 2 #{term.chomp.to_s} #{keybinding_file}` + "\r"
-
+  keybinding_files.each do |file|
+    puts %$Searching "#{File.basename(file.to_s)}"$.on_white + "\n \n"
+    result =  `egrep -B 1 -A 1 #{term.to_s} #{file.to_s}` + "\n \n"
+    if result.strip.empty?
+      puts "#{term}".blue.on_white +  " not matched \n \n"
+    else
+      output = result.gsub(/#{term}/,"#{term}".blue.on_white)
+      puts output + "\n"
+    end
+  end
+  puts "quit! to exit \n \n"
+  puts "Enter Search Term".green
 end
